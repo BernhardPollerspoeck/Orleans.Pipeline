5650e700164ecfe4f2a8817efc0d80b9479b833b
Ledjon Behluli <behluliledjon@hotmail.com>
2024-12-25 01:50:32 +0100

    make data item immutable to avoid deep-copy + generate serializer for enum

--- src/Orleans.Pipeline.Shared/PipeTransferItem.cs
+++ src/Orleans.Pipeline.Shared/PipeTransferItem.cs
@@ -1,5 +1,5 @@
 ﻿namespace Orleans.Pipeline.Shared;
 
-[GenerateSerializer]
+[GenerateSerializer, Immutable]
 [Alias("Orleans.Pipeline.Shared.PipeTransferItem`1")]
 public readonly record struct PipeTransferItem<T>(TransferMode Mode, T? Item, string Metadata);
